fb38e4f04eb6e710244f68e2a99801f665b3cbae
Martin Heidegger <martin.heidegger@gmail.com>
2017-02-22 02:47:33 +0900

    fix(compatibility: Used arguments instead of pipes
    Some systems (ami linux) had problems processing the stdin in the bash script.

--- sync.sh
+++ sync.sh
@@ -8,29 +8,26 @@ remote_sha() {
 if [ ! -f "$LOCK" ]; then
   touch "$LOCK"
   echo "Getting heads"
-  while IFS= read -r heads
-  do
-    HEAD=$(echo ${heads} | awk '{ print $1 }')
-    TARGET_HEAD=$(echo ${heads} | awk '{ print $2 }')
-    if [ -z "${TARGET_HEAD}" ]; then
-      TARGET_HEAD=${HEAD}
-    fi
-    echo "${HEAD}:${TARGET_HEAD}"
-    echo "Checking Branch ${HEAD}"
-    ORIGIN="$(remote_sha origin ${HEAD})"
-    echo "... origin ${HEAD}: ${ORIGIN}"
-    SYNC="$(remote_sha sync $TARGET_HEAD)"
-    echo "... sync ${TARGET_HEAD}: ${SYNC}"
-    if [ "${SYNC}" != "${ORIGIN}" ]; then
-      echo "... synching"
-      git reset --hard origin/$HEAD            || (rm "$LOCK"; exit 1) || exit 1
-      git push --force sync $HEAD:$TARGET_HEAD || (rm "$LOCK"; exit 1) || exit 1
-      echo "... done."
-    else
-      echo "... in sync."
-    fi
-    echo ""
-  done < "${1:-/dev/stdin}"
+  HEAD=${1}
+  TARGET_HEAD=${2}
+  if [ -z "${TARGET_HEAD}" ]; then
+    TARGET_HEAD=${HEAD}
+  fi
+  echo "${HEAD}:${TARGET_HEAD}"
+  echo "Checking Branch ${HEAD}"
+  ORIGIN="$(remote_sha origin ${HEAD})"
+  echo "... origin ${HEAD}: ${ORIGIN}"
+  SYNC="$(remote_sha sync $TARGET_HEAD)"
+  echo "... sync ${TARGET_HEAD}: ${SYNC}"
+  if [ "${SYNC}" != "${ORIGIN}" ]; then
+    echo "... synching"
+    git reset --hard origin/$HEAD            || (rm "$LOCK"; exit 1) || exit 1
+    git push --force sync $HEAD:$TARGET_HEAD || (rm "$LOCK"; exit 1) || exit 1
+    echo "... done."
+  else
+    echo "... in sync."
+  fi
+  echo ""
   rm "$LOCK"
 else
   echo "System locked." >&2
