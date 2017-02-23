418adc4beeed6c9380720ed18e5e190dc273ed7b
Martin Heidegger <martin.heidegger@gmail.com>
2017-02-23 22:51:21 +0900

    feat(setup): changed to different setup for doing the pushes

--- sync.sh
+++ sync.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
-LOCK=".lock"
 pwd
-remote_sha() {
-  (git ls-remote --quiet $1 refs/heads/$2 | awk '{ print $1 }') || return 1
+local_branches() {
+  # https://stackoverflow.com/questions/12370714/git-how-do-i-list-only-local-branches
+  git branch -a | grep -v remotes | awk -F ' +' '! /\(no branch\)/ {print $2}' || exit 1
 }
 
-if [ ! -f "$LOCK" ]; then
-  touch "$LOCK"
-  echo "Getting heads"
-  HEAD=${1}
-  TARGET_HEAD=${2}
-  if [ -z "${TARGET_HEAD}" ]; then
-    TARGET_HEAD=${HEAD}
-  fi
-  echo "${HEAD}:${TARGET_HEAD}"
-  echo "Checking Branch ${HEAD}"
-  ORIGIN="$(remote_sha origin ${HEAD})"
-  echo "... origin ${HEAD}: ${ORIGIN}"
-  SYNC="$(remote_sha sync $TARGET_HEAD)"
-  echo "... sync ${TARGET_HEAD}: ${SYNC}"
-  if [ "${SYNC}" != "${ORIGIN}" ]; then
-    echo "... synching"
-    git reset --hard origin/$HEAD            || (rm "$LOCK"; exit 1) || exit 1
-    git push --force sync $HEAD:$TARGET_HEAD || (rm "$LOCK"; exit 1) || exit 1
-    echo "... done."
-  else
-    echo "... in sync."
-  fi
-  echo ""
+if [ ! -f "${LOCK}" ]; then
+  echo data >${LOCK}
+  echo "$ git fetch origin"
+  git fetch origin
+  for branch in $(local_branches) ; do
+    # https://stackoverflow.com/questions/1628088/reset-local-repository-branch-to-be-just-like-remote-repository-head
+    echo "$ git checkout ${branch}"
+    git checkout ${branch}
+    echo "$ git clean -d --force"
+    git clean -d --force
+    echo "$ git reset --hard origin/${branch}"
+    git reset --hard origin/${branch}
+  done
+  echo "$ git push sync --all --force"
+  git push sync --all --force
   rm "$LOCK"
 else
   echo "System locked." >&2
