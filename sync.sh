3bc0e33481f6b055951bfc197c143f5ef0f535e8
Martin Heidegger <martin.heidegger@gmail.com>
2017-02-23 23:13:21 +0900

    feat(setup): syncing all branches

--- sync.sh
+++ sync.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 pwd
-local_branches() {
-  # https://stackoverflow.com/questions/12370714/git-how-do-i-list-only-local-branches
-  git branch -a | grep -v remotes | awk -F ' +' '! /\(no branch\)/ {print $2}' || exit 1
+origin_branches() {
+  git branch -r | grep origin/ | awk -F/ '{print $2}' || exit 1
 }
 
 if [ ! -f "${LOCK}" ]; then
   echo data >${LOCK}
-  echo "$ git fetch origin"
-  git fetch origin
-  for branch in $(local_branches) ; do
+  echo "$ git fetch --tags --verbose origin"
+  git fetch --tags --verbose origin
+  for branch in $(origin_branches) ; do
     # https://stackoverflow.com/questions/1628088/reset-local-repository-branch-to-be-just-like-remote-repository-head
     echo "$ git checkout ${branch}"
     git checkout ${branch}
